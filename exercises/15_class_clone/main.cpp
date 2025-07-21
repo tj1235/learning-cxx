8babcec1f7b2a12a9a759d8799e1e3af16aa4e62
junt <1804415871@qq.com>
2025-07-21 22:20:04 +0800

    second complete

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -1,4 +1,5 @@
 #include "../exercise.h"
+#include <cstring>
 
 // READ: 复制构造函数 <https://zh.cppreference.com/w/cpp/language/copy_constructor>
 // READ: 函数定义（显式弃置）<https://zh.cppreference.com/w/cpp/language/function>
@@ -16,11 +17,8 @@ public:
     }
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &other){
-        cache = new size_t[other.cached + 1];
-        for (int i = 0; i <= other.cached; ++i) {
-            cache[i] = other.cache[i];
-        }
+    DynFibonacci(DynFibonacci const &other): cache(new size_t[other.cached]), cached(other.cached){
+        std::memcpy(cache, other.cache, cached * sizeof(size_t));
     }
 
     // TODO: 实现析构器，释放缓存空间
